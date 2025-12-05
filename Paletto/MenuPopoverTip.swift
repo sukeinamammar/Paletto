327b083123ced16403cc1298e8d35f763a51ff8e
sukeina <smammar@umich.edu>
2025-12-05 16:20:12 -0500

    added landscape mode. made sure to test it as well.

---
+++ Paletto/MenuPopoverTip.swift
@@ -0,0 +1,18 @@
+//
+//  MenuPopoverTip.swift
+//  Paletto
+//
+//  Created by Sukeina Ammar on 12/4/25.
+//
+
+import SwiftUI
+
+struct MenuPopoverTip: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuPopoverTip()
+}
---
+++ Paletto/NewWelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  NewWelcomePage.swift
+//  Paletto
+//
+//  Created by Sukeina Ammar on 12/4/25.
+//
+
+import SwiftUI
+
+struct NewWelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewWelcomePage()
+}
